24b3720b573bac930a01c0c2866a0a1636996fc7
Luke Underwood <veryveryeasily@gmail.com>
2021-06-28 03:14:34 -0700

    Almost erady

---
+++ Assets/_Game/Code/Liver/Startup.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Startup : SerializedMonoBehaviour {
+    private void Start() {
+        Debug.Log("Startup: Start()");
+    }
+}
---
+++ Assets/_Game/Data/XR/README.md
@@ -0,0 +1,2 @@
+I am near positive we don't need this folder, but Shapes weirdly automatically installs it.
+- Luke
